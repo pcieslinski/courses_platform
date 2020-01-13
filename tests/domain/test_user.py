b1bd6971e0b3d1ecb24cfac4418fdb9e63e0a9c3
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-13 21:24:27 +0100

    Add tests for User entity

---
+++ tests/domain/test_user.py
@@ -0,0 +1,24 @@
+from courses_platform.domain.user import User
+
+
+class TestUserEntity:
+
+    def test_user_initialize_correctly(self):
+        user = User('test@gmail.com')
+
+        assert isinstance(user, User)
+        assert hasattr(user, 'id')
+
+        assert user.email == 'test@gmail.com'
+
+    def test_user_initialize_correctly_from_dict(self):
+        user_dict = {
+            'email': 'test@gmail.com'
+        }
+
+        user = User.from_dict(user_dict)
+
+        assert isinstance(user, User)
+        assert hasattr(user, 'id')
+
+        assert user.email == 'test@gmail.com'
