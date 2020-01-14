49798f95451b72f1f2ef6a8403a2c5df9795f62d
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-14 13:26:38 +0100

    Implement GetAllUsersQuery

---
+++ courses_platform/application/user/queries/get_all.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from courses_platform.domain.user import User
+from courses_platform.application.interfaces.icommand_query import ICommandQuery
+
+
+class GetAllUsersQuery(ICommandQuery):
+    def __init__(self, repo):
+        self.repo = repo
+
+    def execute(self) -> List[User]:
+        users = self.repo.get_all_users()
+        return users
