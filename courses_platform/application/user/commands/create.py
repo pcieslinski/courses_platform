159edc94b5f591e671e68c76a05657f89dc51204
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-14 12:59:29 +0100

    Implement inheritance from interface for Commands and Quries

--- courses_platform/application/user/commands/create.py
+++ courses_platform/application/user/commands/create.py
@@ -1,13 +1,14 @@
 from typing import Union
 
 from courses_platform.domain.user import User
+from courses_platform.application.interfaces.icommand_query import ICommandQuery
 
 
 class UserAlreadyExists(Exception):
     pass
 
 
-class CreateUserCommand:
+class CreateUserCommand(ICommandQuery):
     def __init__(self, repo) -> None:
         self.repo = repo
 
