28d84f5a52ebc04b90a77e4c8eab3400df09b236
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-15 11:47:59 +0100

    Add classmethod from_record to the User entity

--- courses_platform/domain/user.py
+++ courses_platform/domain/user.py
@@ -6,10 +6,17 @@ U = TypeVar('U', bound='User')
 
 
 class User:
-    def __init__(self, email: str) -> None:
-        self.id = str(uuid4())
+    def __init__(self, email: str, id: str = None) -> None:
+        self.id = id or str(uuid4())
         self.email = email
 
     @classmethod
     def from_dict(cls: Type[U], adict: dict) -> U:
         return cls(adict['email'])
+
+    @classmethod
+    def from_record(cls: Type[U], record) -> U:
+        return cls(
+            email=record['email'],
+            id=record['id']
+        )
