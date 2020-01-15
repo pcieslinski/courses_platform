d3d9f42c0e9e8294c37bc28ae3465bc4bf3f7f32
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-15 11:49:23 +0100

    Add tests for from_record classmethod in the User entity

--- tests/domain/test_user.py
+++ tests/domain/test_user.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from courses_platform.domain.user import User
 
 
@@ -21,4 +23,19 @@ class TestUserEntity:
         assert isinstance(user, User)
         assert hasattr(user, 'id')
 
+        assert user.email == 'test@gmail.com'
+
+    def test_user_initialize_correctly_from_record(self):
+        user_id = str(uuid4())
+        user_record = {
+            'id': user_id,
+            'email': 'test@gmail.com'
+        }
+
+        user = User.from_record(user_record)
+
+        assert isinstance(user, User)
+        assert hasattr(user, 'id')
+
+        assert user.id == user_id
         assert user.email == 'test@gmail.com'
