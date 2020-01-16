ae0b18bf4e7ac676846352faa4c981f27af60bef
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-16 14:58:50 +0100

    Implement interface for CourseRepository

---
+++ courses_platform/application/interfaces/icourse_repository.py
@@ -0,0 +1,22 @@
+import abc
+from typing import List, NewType
+
+from courses_platform.domain.course import Course
+
+
+class ICourseRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def create_course(self, name: str) -> Course:
+        pass
+
+    @abc.abstractmethod
+    def delete_course(self, course_id: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_all_courses(self) -> List[Course]:
+        pass
+
+
+CRepository = NewType('CRepository', ICourseRepository)
