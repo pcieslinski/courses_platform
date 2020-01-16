831b5fd0144dd324182b20824564cca206114e3c
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-16 16:19:25 +0100

    Implement CourseRepository

---
+++ courses_platform/persistence/repositories/course/course_repository.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from courses_platform.domain.course import Course
+from courses_platform.persistence.database import Session
+from courses_platform.persistence.repositories.course import course_model as cm
+from courses_platform.application.interfaces.icourse_repository import ICourseRepository
+
+
+class CourseRepository(ICourseRepository):
+    def __init__(self, db_session: Session) -> None:
+        self.db_session = db_session
+
+    def create_course(self, name: str) -> Course:
+        with self.db_session() as db:
+            course = Course(name=name)
+
+            db.add(
+                cm.Course(
+                    id=course.id,
+                    name=course.name
+                )
+            )
+
+            return course
+
+    def delete_course(self, course_id: str) -> bool:
+        with self.db_session() as db:
+            result = db.query(cm.Course).\
+                        filter(cm.Course.id == course_id).\
+                        delete()
+
+            return result
+
+    def get_all_courses(self) -> List[Course]:
+        with self.db_session() as db:
+            result = db.query(cm.Course).\
+                        all()
+
+            courses = [
+                Course.from_record(course_record)
+                for course_record in result
+            ]
+
+            return courses
