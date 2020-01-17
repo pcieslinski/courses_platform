00ef56718ef5c15cdc2c54592b49707b443ecec6
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-17 23:51:55 +0100

    Fix tests

--- tests/service/user/test_users_api.py
+++ tests/service/user/test_users_api.py
@@ -1,6 +1,7 @@
 import json
 import mock
 
+from courses_platform.response_objects import ResponseSuccess
 from courses_platform.serializers.json_user_serializer import UserJsonEncoder
 
 
@@ -8,7 +9,7 @@ class TestUsersApi:
 
     @mock.patch('courses_platform.application.user.queries.get_all.GetAllUsersQuery')
     def test_users_api_returns_list_of_users(self, mock_query, client, users):
-        mock_query().execute.return_value = users
+        mock_query().execute.return_value = ResponseSuccess(value=users)
 
         http_response = client.get('/api/users')
         users_data = json.dumps(users, cls=UserJsonEncoder)
@@ -20,7 +21,7 @@ class TestUsersApi:
 
     @mock.patch('courses_platform.application.user.queries.get_all.GetAllUsersQuery')
     def test_users_api_returns_empty_list_of_users(self, mock_query, client):
-        mock_query().execute.return_value = []
+        mock_query().execute.return_value = ResponseSuccess(value=[])
 
         http_response = client.get('/api/users')
 
@@ -31,7 +32,7 @@ class TestUsersApi:
 
     @mock.patch('courses_platform.application.user.commands.create.CreateUserCommand')
     def test_users_api_creates_new_user(self, mock_command, client, user):
-        mock_command().execute.return_value = user
+        mock_command().execute.return_value = ResponseSuccess(value=user)
 
         mimetype = 'application/json'
         headers = {
@@ -43,7 +44,10 @@ class TestUsersApi:
         http_response = client.post('/api/users', data=data, headers=headers)
         user_data = json.dumps(user, cls=UserJsonEncoder)
 
+        _, kwargs = mock_command().execute.call_args
+
         assert json.loads(http_response.data.decode('UTF-8')) == json.loads(user_data)
-        mock_command().execute.assert_called_with('test@gmail.com')
-        assert http_response.status_code == 201
+        mock_command().execute.assert_called()
+        assert kwargs['request'].email == 'test@gmail.com'
+        assert http_response.status_code == 200
         assert http_response.mimetype == 'application/json'
