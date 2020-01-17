e236d4e8aac59d4eb440844c0f76891c8f35374d
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-18 00:21:26 +0100

    Implement error management in the CoursesApi

--- courses_platform/service/course/courses_api.py
+++ courses_platform/service/course/courses_api.py
@@ -4,8 +4,11 @@ from flask_restful import Resource
 
 from courses_platform.application.course.commands import create
 from courses_platform.application.course.queries import get_all
-from courses_platform.serializers.json_course_serializer import CourseJsonEncoder
 from courses_platform.application.interfaces.icourse_repository import CRepository
+
+from courses_platform.service.status_codes import STATUS_CODES
+from courses_platform.request_objects.course import CreateCourseRequest
+from courses_platform.serializers.json_course_serializer import CourseJsonEncoder
 
 
 class CoursesApi(Resource):
@@ -15,23 +18,23 @@ class CoursesApi(Resource):
     def get(self) -> Response:
         query = get_all.GetAllCoursesQuery(repo=self.repo)
 
-        result = query.execute()
+        response = query.execute()
 
         return Response(
-            json.dumps(result, cls=CourseJsonEncoder),
+            json.dumps(response.value, cls=CourseJsonEncoder),
             mimetype='application/json',
-            status=200
+            status=STATUS_CODES[response.type]
         )
 
     def post(self) -> Response:
-        req_data = request.get_json()
+        request_object = CreateCourseRequest.from_dict(request.get_json())
 
         command = create.CreateCourseCommand(repo=self.repo)
 
-        result = command.execute(req_data['name'])
+        response = command.execute(request=request_object)
 
         return Response(
-            json.dumps(result, cls=CourseJsonEncoder),
+            json.dumps(response.value, cls=CourseJsonEncoder),
             mimetype='application/json',
-            status=201
+            status=STATUS_CODES[response.type]
         )
