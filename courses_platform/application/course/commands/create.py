f807a07799d66dbbcdcedccf8100d139757ad9da
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-17 22:24:15 +0100

    Implement usage of CreateCourseRequest in the CreateCourseCommand

--- courses_platform/application/course/commands/create.py
+++ courses_platform/application/course/commands/create.py
@@ -1,21 +1,20 @@
-from typing import Union
+from courses_platform.request_objects import Request
+from courses_platform.response_objects import Response, ResponseFailure, ResponseSuccess
 
-from courses_platform.domain.course import Course
 from courses_platform.application.interfaces.icommand_query import ICommandQuery
 from courses_platform.application.interfaces.icourse_repository import CRepository
-
-
-class CourseAlreadyExists(Exception):
-    pass
 
 
 class CreateCourseCommand(ICommandQuery):
     def __init__(self, repo: CRepository) -> None:
         self.repo = repo
 
-    def execute(self, name: str) -> Union[Course, Exception]:
+    def execute(self, request: Request) -> Response:
+        if not request:
+            return ResponseFailure.build_from_invalid_request(request)
+
         try:
-            new_course = self.repo.create_course(name=name)
-            return new_course
-        except CourseAlreadyExists as exc:
-            return exc
+            new_course = self.repo.create_course(name=request.name)
+            return ResponseSuccess(new_course)
+        except Exception as exc:
+            return ResponseFailure.build_resource_error(exc)
