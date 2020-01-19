d06f3a96175d479086ce7aa910f07d8cef5dcc30
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-19 17:11:11 +0100

    Add conftest files for application layer

---
+++ tests/application/course/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from typing import Tuple
+from mock import Mock, patch
+from dataclasses import dataclass
+
+
+@pytest.fixture
+def db(course_record: dataclass) -> Mock:
+    db = Mock()
+
+    db.query.return_value.filter.return_value.delete.return_value = 1
+    db.query.return_value.filter.return_value.first.return_value = course_record('1', 'Test Course')
+    db.query.return_value.all.return_value = [
+        course_record('1', 'Test Course'),
+        course_record('2', 'Sample Course')
+    ]
+
+    return db
+
+
+@pytest.fixture(scope='function')
+@patch('courses_platform.persistence.database.session', autospec=True)
+def mock_session_with_db(session: Mock, db: Mock) -> Tuple[Mock, Mock]:
+    session.return_value.__enter__.return_value = db
+    return session, db
