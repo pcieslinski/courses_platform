40c14e97f9aacc048d785267b13ec3d991e83f15
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-19 23:37:36 +0100

    Add tests for EnrollmentsDetailApi

---
+++ tests/service/course/test_enrollments_detail_api.py
@@ -0,0 +1,22 @@
+import json
+import mock
+
+from courses_platform.response_objects import ResponseSuccess
+
+
+class TestEnrollmentsDetailApi:
+
+    @mock.patch('courses_platform.application.course.commands.withdraw_user_enrollment.WithdrawUserEnrollmentCommand')
+    def test_enrollments_detail_api_withdraws_user_enrollment(self, mock_command, client):
+        response = ResponseSuccess.build_response_no_content()
+        mock_command().execute.return_value = response
+
+        http_response = client.delete('/api/courses/123/users/20')
+
+        _, kwargs = mock_command().execute.call_args
+
+        mock_command().execute.assert_called()
+        assert kwargs['request'].course_id == '123'
+        assert kwargs['request'].user_id == '20'
+        assert http_response.data == b''
+        assert http_response.status_code == 204
