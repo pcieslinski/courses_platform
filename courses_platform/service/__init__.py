4e4784e9fb6d3bdb3a29323bd023efd1658a676a
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-19 17:10:30 +0100

    Adapt REST API layer for changes in the persistence layer

--- courses_platform/service/__init__.py
+++ courses_platform/service/__init__.py
@@ -8,12 +8,8 @@ from courses_platform.service.user.users_detail_api import UsersDetailApi
 from courses_platform.service.course.courses_detail_api import CoursesDetailApi
 from courses_platform.service.config import Config, DevConfig
 
-from courses_platform.application.interfaces.iuser_repository import URepository
-from courses_platform.application.interfaces.icourse_repository import CRepository
-
 from courses_platform.persistence.database import session
-from courses_platform.persistence.repositories.user.user_repository import UserRepository
-from courses_platform.persistence.repositories.course.course_repository import CourseRepository
+from courses_platform.application.interfaces.idb_session import DbSession
 
 
 def create_app(config_object: Type[Config] = DevConfig) -> Flask:
@@ -21,46 +17,38 @@ def create_app(config_object: Type[Config] = DevConfig) -> Flask:
     api = Api(app)
 
     app.config.from_object(config_object)
-
-    user_repo, course_repo = create_repositories()
-    register_resources(api, user_repo, course_repo)
+    register_resources(api, session)
 
     return app
 
 
-def register_resources(api: Api, user_repo: URepository, course_repo: CRepository) -> None:
+def register_resources(api: Api, db_session: DbSession) -> None:
     api.add_resource(
         UsersApi,
         '/api/users',
         resource_class_kwargs={
-            'repo': user_repo
+            'db_session': db_session
         }
     )
     api.add_resource(
         UsersDetailApi,
         '/api/users/<user_id>',
         resource_class_kwargs={
-            'repo': user_repo
+            'db_session': db_session
         }
     )
     api.add_resource(
         CoursesApi,
         '/api/courses',
         resource_class_kwargs={
-            'repo': course_repo
+            'db_session': db_session
         }
     )
     api.add_resource(
         CoursesDetailApi,
         '/api/courses/<course_id>',
         resource_class_kwargs={
-            'repo': course_repo
+            'db_session': db_session
         }
     )
 
-
-def create_repositories() -> Tuple[URepository, CRepository]:
-    user_repo = UserRepository(db_session=session)
-    course_repo = CourseRepository(db_session=session)
-
-    return user_repo, course_repo
--- courses_platform/service/user/users_detail_api.py
+++ courses_platform/service/user/users_detail_api.py
@@ -3,20 +3,20 @@ from flask import Response
 from flask_restful import Resource
 
 from courses_platform.application.user.commands import delete
-from courses_platform.application.interfaces.iuser_repository import URepository
+from courses_platform.application.interfaces.idb_session import DbSession
 
 from courses_platform.service.status_codes import STATUS_CODES
 from courses_platform.request_objects.user import DeleteUserRequest
 
 
 class UsersDetailApi(Resource):
-    def __init__(self, repo: URepository) -> None:
-        self.repo = repo
+    def __init__(self, db_session: DbSession) -> None:
+        self.db_session = db_session
 
     def delete(self, user_id) -> Response:
         request_object = DeleteUserRequest.from_dict(dict(user_id=user_id))
 
-        command = delete.DeleteUserCommand(repo=self.repo)
+        command = delete.DeleteUserCommand(db_session=self.db_session)
 
         response = command.execute(request=request_object)
 
