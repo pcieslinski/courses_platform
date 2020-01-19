c83b0567e13bdc6c65b47e25cacd090afe70c99e
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-19 17:08:55 +0100

    Adapt User Commands and Queries for changes in the persistance layer

--- courses_platform/application/user/commands/create.py
+++ courses_platform/application/user/commands/create.py
@@ -1,20 +1,32 @@
 from courses_platform.request_objects import Request
 from courses_platform.response_objects import Response, ResponseFailure, ResponseSuccess
 
+from courses_platform.domain.user import User
+from courses_platform.persistence.database import user as um
+from courses_platform.application.interfaces.idb_session import DbSession
 from courses_platform.application.interfaces.icommand_query import ICommandQuery
-from courses_platform.application.interfaces.iuser_repository import URepository
 
 
 class CreateUserCommand(ICommandQuery):
-    def __init__(self, repo: URepository) -> None:
-        self.repo = repo
+    def __init__(self, db_session: DbSession) -> None:
+        self.db_session = db_session
 
     def execute(self, request: Request) -> Response:
         if not request:
             return ResponseFailure.build_from_invalid_request(request)
 
         try:
-            new_user = self.repo.create_user(email=request.email)
+            with self.db_session() as db:
+                new_user = User(email=request.email)
+
+                db.add(
+                    um.User(
+                        id=new_user.id,
+                        email=new_user.email
+                    )
+                )
+
             return ResponseSuccess.build_response_resource_created(new_user)
+
         except Exception as exc:
             return ResponseFailure.build_resource_error(exc)
