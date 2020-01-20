5a8104dc70069f756dfa31bb258ea5bfbfc8461f
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-20 15:01:27 +0100

    Refactor ValidRequest

--- courses_platform/request_objects/valid_request.py
+++ courses_platform/request_objects/valid_request.py
@@ -1,10 +1,26 @@
-from typing import Type, TypeVar
+from typing import Tuple, Type, TypeVar
+
+from courses_platform.request_objects.invalid_request import InvalidRequest
 
 
 VR = TypeVar('VR', bound='ValidRequest')
 
 
 class ValidRequest:
+    required_params: Tuple[str] = ()
+
+    @classmethod
+    def validate_required_params(
+            cls: Type[VR], invalid_req: InvalidRequest, params: dict) -> InvalidRequest:
+
+        for required_param in cls.required_params:
+            if required_param not in params:
+                invalid_req.add_error(
+                    required_param,
+                    f'{required_param} is a required parameter'
+                )
+        return invalid_req
+
     @classmethod
     def from_dict(cls: Type[VR], params: dict) -> None:
         raise NotImplementedError
