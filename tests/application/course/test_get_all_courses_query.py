dec964188f60d08c9678f9d3bcc992b78f9b9ccd
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-20 19:32:35 +0100

    Fix tests for GetAllCoursesQuery

--- tests/application/course/test_get_all_courses_query.py
+++ tests/application/course/test_get_all_courses_query.py
@@ -1,8 +1,10 @@
 import pytest
-from mock import Mock
+from mock import Mock, patch
 from typing import Tuple
 
+from courses_platform.domain.course import Course
 from courses_platform.response_objects import ResponseSuccess
+from courses_platform.request_objects.course import GetAllCoursesRequest
 from courses_platform.application.course.queries.get_all import GetAllCoursesQuery
 from courses_platform.application.interfaces.icommand_query import CommandQuery
 
@@ -28,7 +30,7 @@ class TestGetAllCoursesQuery:
     def test_get_all_courses_query_executes_correctly(self, get_all_query_with_mocks):
         query, mock_session, db = get_all_query_with_mocks
 
-        response = query.execute()
+        response = query.execute(request=GetAllCoursesRequest())
 
         mock_session.assert_called_once()
         db.query().all.assert_called_once_with()
@@ -41,3 +43,23 @@ class TestGetAllCoursesQuery:
         assert response.value[0].name == 'Test Course'
         assert response.value[1].id == '2'
         assert response.value[1].name == 'Sample Course'
+
+    @patch('courses_platform.application.course.queries.get_all.GetAllCoursesQuery._create_courses_objects_with_stats')
+    def test_get_all_courses_query_returns_courses_with_stats(self, mock_creat_courses_objects_with_stats,
+                                                              get_all_query_with_mocks,
+                                                              courses_with_enrollments):
+        mock_creat_courses_objects_with_stats.return_value = courses_with_enrollments
+
+        query, mock_session, db = get_all_query_with_mocks
+
+        response = query.execute(request=GetAllCoursesRequest(include=['stats']))
+
+        mock_session.assert_called_once()
+        db.query().all.assert_called_once_with()
+        mock_creat_courses_objects_with_stats.assert_called_once()
+
+        assert isinstance(response, ResponseSuccess)
+        assert response.type == ResponseSuccess.SUCCESS_OK
+
+        assert len(response.value) == 1
+        assert response.value == courses_with_enrollments
