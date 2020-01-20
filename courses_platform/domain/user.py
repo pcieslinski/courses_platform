c8baeb21df7639debf97d34832a17f64bde6a634
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-20 01:18:29 +0100

    Add courses field to the User entity

--- courses_platform/domain/user.py
+++ courses_platform/domain/user.py
@@ -1,14 +1,16 @@
 from uuid import uuid4
-from typing import Type, TypeVar
+from typing import List, Type, TypeVar
 
+from courses_platform.domain.course import Course
 
 U = TypeVar('U', bound='User')
 
 
 class User:
-    def __init__(self, email: str, id: str = None) -> None:
+    def __init__(self, email: str, id: str = None, courses: List[Course] = None) -> None:
         self.id = id or str(uuid4())
         self.email = email
+        self.courses = courses or []
 
     @classmethod
     def from_dict(cls: Type[U], adict: dict) -> U:
@@ -18,5 +20,6 @@ class User:
     def from_record(cls: Type[U], record) -> U:
         return cls(
             email=record.email,
-            id=record.id
+            id=record.id,
+            courses=record.courses
         )
