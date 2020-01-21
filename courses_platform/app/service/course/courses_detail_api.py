4b031830c528ab388e1ee098367cdbb4a780c766
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-21 20:58:30 +0100

    Implement GetCourseQuery in the REST API

--- courses_platform/app/service/course/courses_detail_api.py
+++ courses_platform/app/service/course/courses_detail_api.py
@@ -2,16 +2,31 @@ import json
 from flask import Response
 from flask_restful import Resource
 
+from app.application.course.queries import get
 from app.application.course.commands import delete
 from app.application.interfaces.idb_session import DbSession
 
 from app.service.status_codes import STATUS_CODES
-from app.request_objects.course import DeleteCourseRequest
+from app.serializers.json_course_serializer import CourseJsonEncoder
+from app.request_objects.course import DeleteCourseRequest, GetCourseRequest
 
 
 class CoursesDetailApi(Resource):
     def __init__(self, db_session: DbSession) -> None:
         self.db_session = db_session
+
+    def get(self, course_id: str) -> Response:
+        request_object = GetCourseRequest.from_dict(dict(course_id=course_id))
+
+        query = get.GetCourseQuery(db_session=self.db_session)
+
+        response = query.execute(request=request_object)
+
+        return Response(
+            json.dumps(response.value, cls=CourseJsonEncoder),
+            mimetype='application/json',
+            status=STATUS_CODES[response.type]
+        )
 
     def delete(self, course_id) -> Response:
         request_object = DeleteCourseRequest.from_dict(dict(course_id=course_id))
