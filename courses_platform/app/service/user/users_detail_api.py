25b712b1270e7e6bcfe8ba2c29a296dc8af527d2
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-21 20:59:10 +0100

    Implement GetUserQuery in the REST API

--- courses_platform/app/service/user/users_detail_api.py
+++ courses_platform/app/service/user/users_detail_api.py
@@ -2,18 +2,33 @@ import json
 from flask import Response
 from flask_restful import Resource
 
+from app.application.user.queries import get
 from app.application.user.commands import delete
 from app.application.interfaces.idb_session import DbSession
 
 from app.service.status_codes import STATUS_CODES
-from app.request_objects.user import DeleteUserRequest
+from app.serializers.json_user_serializer import UserJsonEncoder
+from app.request_objects.user import GetUserRequest, DeleteUserRequest
 
 
 class UsersDetailApi(Resource):
     def __init__(self, db_session: DbSession) -> None:
         self.db_session = db_session
 
-    def delete(self, user_id) -> Response:
+    def get(self, user_id: str) -> Response:
+        request_object = GetUserRequest.from_dict(dict(user_id=user_id))
+
+        query = get.GetUserQuery(db_session=self.db_session)
+
+        response = query.execute(request=request_object)
+
+        return Response(
+            json.dumps(response.value, cls=UserJsonEncoder),
+            mimetype='application/json',
+            status=STATUS_CODES[response.type]
+        )
+
+    def delete(self, user_id: str) -> Response:
         request_object = DeleteUserRequest.from_dict(dict(user_id=user_id))
 
         command = delete.DeleteUserCommand(db_session=self.db_session)
