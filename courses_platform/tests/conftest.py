3d365f85e76af151842f9d7f835745a58fd36226
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-05-10 17:06:44 +0200

    Add End 2 End test

--- courses_platform/tests/conftest.py
+++ courses_platform/tests/conftest.py
@@ -1,12 +1,16 @@
-import pytest
+import os
+import time
 from typing import Dict, Generator, List, Union
 
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker, clear_mappers, Session
 
 from app.domain.user import User
 from app.domain.course import Course
+from app.persistence import DB_PATH
 from app.persistence.orm import metadata, start_mappers
 from app.persistence.unit_of_work import SqlAlchemyUnitOfWork
 
@@ -75,3 +79,38 @@ def session(session_factory) -> Session:
 @pytest.fixture
 def uow(session_factory) -> SqlAlchemyUnitOfWork:
     return SqlAlchemyUnitOfWork(session_factory)
+
+
+def postgres_is_responsive(engine: Engine) -> bool:
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            engine.connect()
+            return True
+        except OperationalError:
+            time.sleep(0.5)
+    return False
+
+
+@pytest.fixture(scope='session')
+def postgres_db() -> Engine:
+    engine = create_engine(DB_PATH)
+
+    if not postgres_is_responsive(engine):
+        pytest.fail('Postgres never came up')
+
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def postgres_session_factory(postgres_db) -> Generator[sessionmaker, None, None]:
+    start_mappers()
+    yield sessionmaker(bind=postgres_db)
+    clear_mappers()
+
+
+@pytest.fixture
+def api_base_url() -> str:
+    host = os.environ.get('API_HOST', 'localhost')
+    return f'http://{host}:5000'
---
+++ courses_platform/tests/e2e/test_withdraw_user_enrollment.py
@@ -0,0 +1,61 @@
+import json
+
+import pytest
+import requests
+
+from app.persistence.unit_of_work import SqlAlchemyUnitOfWork
+
+
+def post(api_base_url: str, path: str, data: str) -> requests.Response:
+    mimetype = "application/json"
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype
+    }
+    return requests.post(f'{api_base_url}{path}', data=data, headers=headers)
+
+
+@pytest.mark.usefixtures('postgres_db')
+def test_withdraw_user_enrollment(postgres_session_factory, api_base_url):
+    user_data = json.dumps(dict(email="dev@gmail.com"))
+    user_created_response = post(api_base_url, '/api/users', data=user_data)
+    user_id = json.loads(user_created_response.content)['id']
+
+    course_data = json.dumps(dict(name="Random Course"))
+    course_created_response = post(api_base_url, '/api/courses', data=course_data)
+    course_id = json.loads(course_created_response.content)['id']
+
+    enroll_user_data = json.dumps(dict(user_id=user_id))
+    user_enrolled_response = post(
+        api_base_url,
+        f'/api/courses/{course_id}/users',
+        data=enroll_user_data,
+    )
+
+    uow = SqlAlchemyUnitOfWork(session_factory=postgres_session_factory)
+
+    with uow:
+        user = uow.users.get(user_id)
+        course = uow.courses.get(course_id)
+
+    assert user.id == user_id
+    assert user.email == 'dev@gmail.com'
+    assert course.id == course_id
+    assert course.name == 'Random Course'
+    assert course.is_enrolled(user)
+
+    assert user_enrolled_response.status_code == 201
+    assert json.loads(user_enrolled_response.content) == dict(
+        course_id=course_id,
+        user_id=user_id
+    )
+
+    enrollment_withdrawn_response = requests.delete(
+        f'{api_base_url}/api/courses/{course_id}/users/{user_id}'
+    )
+
+    with uow:
+        course = uow.courses.get(course_id)
+
+    assert list(course.enrollments) == []
+    assert enrollment_withdrawn_response.status_code == 204
