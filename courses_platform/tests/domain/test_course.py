bb5680bc73f16757a3788a12af23694e5c81dbf9
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-05-16 15:35:46 +0200

    Add enrollments_count property to the Course domain model

--- courses_platform/tests/domain/test_course.py
+++ courses_platform/tests/domain/test_course.py
@@ -1,3 +1,4 @@
+from app.domain.user import User
 from app.domain.course import Course
 
 
@@ -10,6 +11,7 @@ class TestCourseEntity:
         assert hasattr(course, 'id')
 
         assert course.name == 'Test Course'
+        assert course.enrollments == []
 
     def test_course_initialize_correctly_from_dict(self):
         course_data = {
@@ -22,3 +24,26 @@ class TestCourseEntity:
         assert hasattr(course, 'id')
 
         assert course.name == 'Test Course'
+        assert course.enrollments == []
+
+    def test_enrollments_count_property_returns_number_of_enrollments(self):
+        course = Course(name='Test Course', enrollments=[
+            User(email='test@gmail.com')
+        ])
+
+        assert course.enrollments_count == 1
+
+    def test_is_enrolled_checks_if_user_is_enrolled_in_course(self):
+        user = User(email='test@gmail.com')
+        course = Course(name='Test Course', enrollments=[user])
+
+        assert course.is_enrolled(user)
+
+    def test_clear_enrollments_removes_all_enrollments_for_a_course(self):
+        user_1 = User('test@gmail.com')
+        user_2 = User('dev@gmail.com')
+        course = Course(name='Test Course', enrollments=[user_1, user_2])
+
+        course.clear_enrollments()
+
+        assert course.enrollments == []
